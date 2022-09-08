28f35806ebdc928523b1739dbd9183837aa0e9b9
odwq <113080013+odwq@users.noreply.github.com>
2022-09-08 11:27:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,16 +6,16 @@ export const config = {
   // 公众号APP_ID 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  // APP_ID: "",
+  // APP_ID: "wx554be36ba0e3b04c",
 
   // 公众号APP_SECRET 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  // APP_SECRET: "",
+  // APP_SECRET: "01b8d8b56991ba94dd01e3dc48b0bc61",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-   * [
+   * [ofBYU52Sqvy9130UFpzGIFjF_2cE
    *  {
    *    // 想要发送的人的名字
    *    name: "老婆0",
@@ -37,11 +37,11 @@ export const config = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
       // 所在省份
-      province: "广东",
+      province: "安徽",
       // 所在城市
-      city: "肇庆",
+      city: "淮安",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://www.douyin.com/"
     }, 
     {
       name: "老婆1", 
@@ -77,7 +77,7 @@ export const config = {
   CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-  CALLBACK_USERS: [
+  CALLBACK_USERS: [ofBYU54FO80O8IH05NjxBzdRc-qA
     {
       // 一般都填自己
       name: "自己",
@@ -93,9 +93,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "安徽",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "淮安",
 
   /** 重要节日相关 */
 
@@ -106,10 +106,9 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "生日", "name": "老婆", "year": "2005", "date": "04-06"},
+    {"type": "节日", "name": "结婚纪念日", "year": "2022", "date": "01-08"},
+    
   ],
 
   /**
@@ -127,13 +126,9 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
-    // 结婚纪念日
-    {"keyword": "marry_day", date: "2020-01-04"},
-    // 退伍日, 不用可以删掉
-    {"keyword": "ex_day", date: "2022-08-31"},
-    // sakana日
-    // {"keyword": "sakana_day", date: "2022-01-06"},
+    {"keyword": "love_day", date: "2022-01-08"},
+   
+ 
     // ... 
   ],
 
@@ -151,7 +146,14 @@ export const config = {
       "因为太喜欢你，所以看谁都像是情敌。",
       "申请成为你爱里的永久居民。",
       "你很傻，你很笨，可我还是很羡慕你，因为你有我",
-      "遇见你，就好像捡到了100斤的运气",
+       "遇见你，就好像捡到了100斤的运气"
+      "你是我一生只会遇见一次的惊喜"
+      "我想你了，不是随便说说的那种想，是在刚刚那个瞬间想跑着去见你的那种想"
+      " 我想对着你碎碎念，分享我的各种无关紧要的小事，分享我的昨天今天明天， 但是我不能，不能让你知道我有这么牵挂你"
+
+
+     
+     ,
     ]},
   ],
 
